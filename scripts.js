09836da4d8947ae8382fcc63a889e626e067871c
Juan Pablo Leyva <jpleyvat@gmail.com>
2019-10-01 22:24:29 -0400

    Adds dot navigation.

--- scripts.js
+++ scripts.js
@@ -1,73 +1,104 @@
+dot(100);
 var _a = [
-		document.getElementsByClassName('page'),
-		document.getElementById('container'),
-		document.getElementsByClassName('page').length
-	],
-	pages = _a[0],
-	container = _a[1],
-	numberOfPages = _a[2];
+    document.getElementsByClassName('page'),
+    document.getElementById('container'),
+    document.getElementsByClassName('page').length,
+    1
+], pages = _a[0], container = _a[1], numberOfPages = _a[2], currentPage = _a[3];
 for (var i = 0; i < pages.length; i++)
-	pages[i].style.top = String(i * 100) + '%';
-document.addEventListener('wheel', function(event) {
-	if (getpPropertyValue(container.style.bottom) % 100 === 0) {
-		if (event.deltaY < 0) scrollUp();
-		if (event.deltaY > 0) scrollDown();
-	}
+    pages[i].style.top = String(i * 100) + '%';
+document.addEventListener('wheel', function (event) {
+    if (getpPropertyValue(container.style.bottom) % 100 === 0) {
+        if (event.deltaY < 0)
+            scrollUp();
+        if (event.deltaY > 0)
+            scrollDown();
+    }
 });
-document.addEventListener('touchmove', function(event) {
-	console.log(event);
+document.addEventListener('touchmove', function (event) {
+    console.log(event);
 });
+var navigation;
+navigation = document.getElementsByClassName('navigation')[0]
+    .children[0].children;
+for (var i = 0; i < navigation.length; i++) {
+    navigation[i].children[0].addEventListener('click', function (event) {
+        var pageNumber = Number(event.target.id.replace('dot', ''));
+        var operation = 1;
+        if (currentPage > pageNumber)
+            operation = -1;
+        var animation = setInterval(frame, 2);
+        var initialPosition = getpPropertyValue(container.style.bottom);
+        var interval = (Math.abs(initialPosition - (pageNumber - 1) * 100) *
+            operation) /
+            100;
+        function frame() {
+            if (getpPropertyValue(container.style.bottom) ===
+                (pageNumber - 1) * 100) {
+                clearInterval(animation);
+                dot(pageNumber * 100);
+            }
+            else {
+                container.style.bottom = modifyPorperty(container.style.bottom, interval);
+            }
+        }
+    });
+}
 function scrollUp() {
-	var initialPosition = getpPropertyValue(container.style.bottom);
-	if (
-		container.style.bottom != '' &&
-		container.style.bottom != '0%'
-	) {
-		var animation_1 = setInterval(frame, 2);
-		function frame() {
-			if (
-				getpPropertyValue(container.style.bottom) ===
-					initialPosition - 100 ||
-				getpPropertyValue(container.style.bottom) <= 0
-			) {
-				clearInterval(animation_1);
-			} else {
-				container.style.bottom = modifyProperty(
-					container.style.bottom,
-					-1
-				);
-			}
-		}
-	}
+    var initialPosition = getpPropertyValue(container.style.bottom);
+    if (container.style.bottom != '' &&
+        container.style.bottom != '0%') {
+        dot(initialPosition);
+        var animation_1 = setInterval(frame, 2);
+        function frame() {
+            if (getpPropertyValue(container.style.bottom) ===
+                initialPosition - 100 ||
+                getpPropertyValue(container.style.bottom) <= 0) {
+                clearInterval(animation_1);
+            }
+            else {
+                container.style.bottom = modifyPorperty(container.style.bottom, -1);
+            }
+        }
+    }
 }
 function scrollDown() {
-	var initialPosition = getpPropertyValue(container.style.bottom);
-	if (
-		getpPropertyValue(container.style.bottom) <=
-		numberOfPages * 100 - 200
-	) {
-		var animation_2 = setInterval(frame, 2);
-		function frame() {
-			if (
-				getpPropertyValue(container.style.bottom) ===
-					initialPosition + 100 ||
-				getpPropertyValue(container.style.bottom) >
-					numberOfPages * 100 - 101
-			) {
-				clearInterval(animation_2);
-			} else {
-				container.style.bottom = modifyProperty(
-					container.style.bottom,
-					1
-				);
-			}
-		}
-	}
+    var initialPosition = getpPropertyValue(container.style.bottom);
+    if (getpPropertyValue(container.style.bottom) <=
+        numberOfPages * 100 - 200) {
+        dot(initialPosition + 200);
+        var animation_2 = setInterval(frame, 2);
+        function frame() {
+            if (getpPropertyValue(container.style.bottom) ===
+                initialPosition + 100 ||
+                getpPropertyValue(container.style.bottom) >
+                    numberOfPages * 100 - 101) {
+                clearInterval(animation_2);
+            }
+            else {
+                container.style.bottom = modifyPorperty(container.style.bottom, 1);
+            }
+        }
+    }
 }
-function modifyProperty(initial, value) {
-	var result = String(getpPropertyValue(initial) + value) + '%';
-	return result;
+function modifyPorperty(initial, value) {
+    var result = String(getpPropertyValue(initial) + value) + '%';
+    return result;
 }
 function getpPropertyValue(property) {
-	return Number(property.replace('%', ''));
+    return Number(property.replace('%', ''));
+}
+function dot(scroll) {
+    var page = scroll / 100;
+    var dot;
+    var navigation;
+    navigation = document.getElementsByClassName('navigation')[0]
+        .children[0].children;
+    for (var _i = 0, navigation_1 = navigation; _i < navigation_1.length; _i++) {
+        var item = navigation_1[_i];
+        item.children[0].src = 'assets/dot.svg';
+    }
+    dot = document.getElementById('dot' + page);
+    dot.src = 'assets/full-dot.svg';
+    currentPage = page;
 }
--- scripts.ts
+++ scripts.ts
@@ -1,22 +1,59 @@
-let [pages, container, numberOfPages]: any = [
+dot(100);
+
+let [pages, container, numberOfPages, currentPage]: any = [
 	document.getElementsByClassName('page'),
 	document.getElementById('container'),
-	document.getElementsByClassName('page').length
+	document.getElementsByClassName('page').length,
+	1
 ];
 
 for (let i = 0; i < pages.length; i++)
 	pages[i].style.top = String(i * 100) + '%';
 
-document.addEventListener('wheel', event => {
+document.addEventListener('wheel', (event) => {
 	if (getpPropertyValue(container.style.bottom) % 100 === 0) {
 		if (event.deltaY < 0) scrollUp();
 		if (event.deltaY > 0) scrollDown();
 	}
 });
 
-document.addEventListener('touchmove', event => {
+document.addEventListener('touchmove', (event) => {
 	console.log(event);
 });
+
+let navigation: any;
+navigation = document.getElementsByClassName('navigation')[0]
+	.children[0].children;
+
+for (let i = 0; i < navigation.length; i++) {
+	navigation[i].children[0].addEventListener('click', (event) => {
+		let pageNumber = Number(event.target.id.replace('dot', ''));
+		let operation = 1;
+		if (currentPage > pageNumber) operation = -1;
+		let animation = setInterval(frame, 2);
+		let initialPosition = getpPropertyValue(
+			container.style.bottom
+		);
+		let interval =
+			(Math.abs(initialPosition - (pageNumber - 1) * 100) *
+				operation) /
+			100;
+		function frame() {
+			if (
+				getpPropertyValue(container.style.bottom) ===
+				(pageNumber - 1) * 100
+			) {
+				clearInterval(animation);
+				dot(pageNumber * 100);
+			} else {
+				container.style.bottom = modifyPorperty(
+					container.style.bottom,
+					interval
+				);
+			}
+		}
+	});
+}
 
 function scrollUp() {
 	let initialPosition = getpPropertyValue(container.style.bottom);
@@ -24,6 +61,7 @@ function scrollUp() {
 		container.style.bottom != '' &&
 		container.style.bottom != '0%'
 	) {
+		dot(initialPosition);
 		let animation = setInterval(frame, 2);
 		function frame() {
 			if (
@@ -48,6 +86,7 @@ function scrollDown() {
 		getpPropertyValue(container.style.bottom) <=
 		numberOfPages * 100 - 200
 	) {
+		dot(initialPosition + 200);
 		let animation = setInterval(frame, 2);
 		function frame() {
 			if (
@@ -74,4 +113,18 @@ function modifyPorperty(initial: string, value: number) {
 
 function getpPropertyValue(property) {
 	return Number(property.replace('%', ''));
+}
+
+function dot(scroll) {
+	let page = scroll / 100;
+	let dot: any;
+	let navigation: any;
+	navigation = document.getElementsByClassName('navigation')[0]
+		.children[0].children;
+	for (let item of navigation) {
+		item.children[0].src = 'assets/dot.svg';
+	}
+	dot = document.getElementById('dot' + page);
+	dot.src = 'assets/full-dot.svg';
+	currentPage = page;
 }
